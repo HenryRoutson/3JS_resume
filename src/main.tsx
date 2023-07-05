51281cbe122360c379261ab0b1d5ac4107a6d54b
Henry Routson <hroutson@student.unimelb.edu.au>
2023-07-05 13:48:03 +1000

    change to orbit controls

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,12 @@ import * as THREE from 'three';
 
 // https://github.com/pmndrs/three-stdlib
 
-import { GLTFLoader, FlyControls } from 'three-stdlib'
+import { GLTFLoader, FlyControls, OrbitControls } from 'three-stdlib'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set( 0, 0.2, 0 );
+camera.rotation.set( 0, 0, 0 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -19,12 +21,7 @@ document.body.appendChild( renderer.domElement );
 
 scene.add( new THREE.DirectionalLight );
 
-let controls = new FlyControls( camera, renderer.domElement );
-controls.movementSpeed = 1;
-controls.rollSpeed = Math.PI / 24;
-controls.autoForward = false;
-controls.dragToLook = true;
-
+let controls = new OrbitControls( camera, renderer.domElement );
 
 const loader = new GLTFLoader();
 
@@ -42,7 +39,7 @@ loader.load( 'Assets/GLTFs/macbook_on_wood_desk.glb', function ( gltf ) {
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-    controls.update(0.01)
+    controls.update();
 }
 
 animate();
